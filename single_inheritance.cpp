79f7c92e716245651df0e942ed57a4970caeacbf
Abhinav0055 <abhinav0055.be21@chitkara.edu.in>
2022-05-20 10:29:42 +0530

    secons commit

--- single_inheritance.cpp
+++ single_inheritance.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 class Base {
   public:
-    float salary = 900;
+    float money = 900;
 };
-class Derived: public Base {
+class salary: public Base {
   public: 
     float bonus = 100;
     void sum() {
-      cout << "Your Total Salary is: " << (salary + bonus) << endl;
+      cout << "Your Total Salary is: " << (money + bonus) << endl;
     }
 };
 int main() {
-  Derived x;
-  cout << "Your Salary is:" << x.salary << endl;
+  salary x;
+  cout << "Your Salary is:" << x.money << endl;
   cout << "Your Bonus is:" << x.bonus << endl;
   x.sum();
   return 0;
