e75145ec6a34be46e19bba0821a5fdb0e15019e4
Aaryan Prabhakar <aaryanprabhakar07@gmail.com>
2022-05-30 16:03:57 +0530

    done changes on another branch

--- single_inheritance.cpp
+++ single_inheritance.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 class Base {
   public:
-    float money = 900;
+    float salary = 900;
 };
 class salary: public Base {
   public: 
     float bonus1 = 100;
     void sum() {
-      cout << "Your Total Salary is: " << (money + bonus1) << endl;
+      cout << "Your Total Salary is: " << (salary + bonus1) << endl;
     }
 };
 int main() {
   salary x;
-  cout << "Your Salary is:" << x.money << endl;
+  cout << "Your Salary is:" << x.salary << endl;
   cout << "Your Bonus is:" << x.bonus1 << endl;
   x.sum();
   return 0;
